5edb00e38d5ab78367a067435ac91d6ee2193dd2
Elliott Alexander <escottalexander@gmail.com>
2018-12-10 12:03:04 -0500

    recommended design changes

--- src/components/Property.js
+++ src/components/Property.js
@@ -5,11 +5,11 @@ import './Property.css';
 export default function Property(props) {
     return (
         <div className="property">
-            {props.data.imgSrc ? <img src={props.data.imgSrc} className="property-img" alt="this property" /> : ""}
             <div className="prop-info">
                 <h3 className="address">{props.data.address}</h3>
                 <p className="short-desc">{props.data.description}</p>
-                <button><Link to={`/dashboard/${props.data.slug}`}>View Property</Link></button>
+                {props.data.imgSrc ? <img src={props.data.imgSrc} className="property-img" alt="this property" /> : ""}
+                <button className="view-property"><Link to={`/dashboard/${props.data.slug}`}>View Property</Link></button>
             </div>
         </div>
     );
